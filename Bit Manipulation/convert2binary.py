72b5e499c655f0678ccda394ab7933973ff95eba
Divesh455 <matkardivesh26@gmail.com>
2026-08-17 17:19:15 +0530

    i to b

---
+++ Bit Manipulation/convert2binary.py
@@ -0,0 +1,19 @@
+
+
+def convert2binary(num:int) -> str:
+    
+    result = ""
+    
+    while num > 0:
+        
+        if num % 2 == 1:
+            result += "1"
+        else:
+            result += "0"
+        
+        num = num // 2
+        
+    result = result[::-1]
+    return result
+
+print(convert2binary(9))
